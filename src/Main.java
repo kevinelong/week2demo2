4097684ab056fca7534d3e683321b1fafe32c0fa
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-14 14:46:56 -0700

    first

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //what we have
+        String fullName = "Kevin Ernest Long";
+
+        //what we want
+        //String[] nameList = {"Kevin", "Ernest", "Long"};
+
+        //how? split!
+        String[] nameList = fullName.split(" ");  // " " (space) is the delimiter, where we cut apart: parse.
+
+        for(String n : nameList){
+            System.out.println(n);
         }
+
+        String[] fruit = {"Apple", "Banana", "Cherry"};
+        String text = String.join(" ", fruit); // glue together
+        System.out.println(text);
+
+        //fast but not too wierd or sophisticated
+        StringBuilder sb = new StringBuilder();
+        sb.append("first");
+        sb.append(" ");
+        sb.append("second");
+        sb.append(" ");
+        sb.append("third");
+        System.out.println(sb);
+
+//        //slow but familar
+//        String body1 = "";
+//        body1 += "first ";
+//        body1 += "second ";
+//        body1 += "third";
+//        System.out.println(body1);
+//
+//        // awkward but faster choice
+//        ArrayList<String> body = new ArrayList<>();
+//        body.add("first");
+//        body.add("second");
+//        body.add("third");
+//        System.out.println(String.join(" ", body));
+
     }
 }
